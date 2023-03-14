e0eb67b2f8fe75847c78ffeff501545b5bde9d5d
jinyounghwa <timotolkie@gmail.com>
2023-03-14 12:44:47 +0900

    2nd 20230314
    헤더,푸터 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const currentDay = new Date();
+  const currentYear = currentDay.getFullYear();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="fixed inset-x-0 top-0 z-50 left-0 bg-white text-gray-700 body-font border-b border-gray-200">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <div
+            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          >
+            {/* <img alt="logo" src={logo} className="w-8 h-8 -mr-1" /> */}
+            <span className="ml-3 text-xl text-indigo-500">기획자 진영화의 개발이야기</span>
+          </div>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <div className="mr-5 hover:text-gray-900">
+              About
+            </div>
+            <div className="mr-5 hover:text-gray-900">
+              JavaScript
+            </div>
+            <div className="mr-5 hover:text-gray-900">
+              Rust
+            </div>
+          </nav>
+        </div>
+      </header>
+      <section className="containe mb-9 mt-24">
+        본문내용
+      </section>
+      <section className="containe mb-8 mt-8">
+        본문내용
+      </section>
+      <section className="containe mb-8 mt-8">
+        본문내용
+      </section>
+      <footer className="text-gray-700 body-font">
+        <div className="bg-gray-200">
+          <div className="container mx-auto pt-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p className="text-gray-500 text-sm text-center sm:text-left">
+              © {currentYear} 진영화
+            </p>
+            <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">
+              timotolkie@gmail.com
+            </span>
+          </div>
+          <div className="container mx-auto pb-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">
+              기획/디자인/개발/컨텐츠 진영화
+            </span>
+          </div>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
